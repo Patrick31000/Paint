411248dda94786f1c081397aa2ca8d85b518dff6
PatTaylor <patrick.taylor31@gmail.com>
2017-01-11 13:51:17 +0100

    Ajout gomme et bouton init

--- js/index.js
+++ js/index.js
@@ -19,10 +19,6 @@ $("input:button").each(function() {
     i++;
 });
 
-
-/*var r = finalX - originX;
-console.log(r);*/
-
 function InitThis() {
     ctx = document.getElementById('paint').getContext("2d");
 
@@ -51,8 +47,6 @@ function InitThis() {
 
 $("input:button").click(function() {
     fond = $(this).attr("data-color");
-    var r = finalX - originX;
-    console.log(r);
 });
 
 function Draw(x, y, isDown) {
@@ -69,57 +63,64 @@ function Draw(x, y, isDown) {
     lastX = x;
     lastY = y;
 
-    $("input:button").click(function() {
-        fond = $(this).attr("data-color");
+    $("button:reset").click(function() {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, paint.width, paint.height);
+    });
+
+
+    $("button:reset").click(function() {
+        var dataURL = paint.toDataURL('image/png');
+        button.href = dataURL;
     });
 
 
 
     /*function InitThis() {
-        ctx = document.getElementById('paint').getContext("2d");
+         ctx = document.getElementById('paint').getContext("2d");
 
         $('#paint').mousedown(function(e) {
-            mousePressed = true;
-            originX = e.pageX - $(this).offset().left;
-            originY = e.pageY - $(this).offset().top;
-            Draw(originX, originY, false);
-        });
+             mousePressed = true;
+             originX = e.pageX - $(this).offset().left;
+             originY = e.pageY - $(this).offset().top;
+             Draw(originX, originY, false);
+         });
 
         $('#paint').mousemove(function(e) {
-            if (mousePressed) {
-                finalX = e.pageX - $(this).offset().left;
-                finalY = e.pageY - $(this).offset().top;
-                Draw(finalX, finalY, true);
-            }
-        });
+             if (mousePressed) {
+                 finalX = e.pageX - $(this).offset().left;
+                 finalY = e.pageY - $(this).offset().top;
+                 Draw(finalX, finalY, true);
+             }
+         });
 
         $('#paint').mouseup(function(e) {
-            mousePressed = false;
-        });
-        $('#paint').mouseleave(function(e) {
-            mousePressed = false;
-        });
-    }
+             mousePressed = false;
+         });
+         $('#paint').mouseleave(function(e) {
+             mousePressed = false;
+         });
+     }
 
     $("input:button").click(function() {
-        fond = $(this).attr("data-color");
+         fond = $(this).attr("data-color");
 
         var r = finalX - originX;
-        console.log(r);
-    });
+         console.log(r);
+     });
 
     function Circle(originX, originY, (finalX - originX), isDown) {
-        if (isDown) {
-            ctx.beginPath();
-            ctx.strokeStyle = fond;
-            ctx.lineWidth = 10; /*$('#selWidth').val();
-            ctx.lineJoin = "round";
-            ctx.moveTo(lastX, lastY);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.stroke();
-        }
-        lastX = x;
-        lastY = y;
-    };*/
+         if (isDown) {
+             ctx.beginPath();
+             ctx.strokeStyle = fond;
+             ctx.lineWidth = 10; /*$('#selWidth').val();
+             ctx.lineJoin = "round";
+             ctx.moveTo(lastX, lastY);
+             ctx.lineTo(x, y);
+             ctx.closePath();
+             ctx.stroke();
+         }
+         lastX = x;
+         lastY = y;
+     };*/
 }
