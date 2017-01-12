e9284063845e05ba66c87f7f4d3552926a96f810
PatTaylor <patrick.taylor31@gmail.com>
2017-01-12 16:56:03 +0100

    Version7

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
     var originY
     var finalX
     var finalY
-    var endX = finalX - originX
-    var endY = finalY - originY
+    var endX
+    var endY
     var ctx = document.getElementById('paint').getContext("2d");
-    var tools = {};
+
 
 
 
@@ -70,12 +70,13 @@ $(document).ready(function() {
         if (isDown) {
             ctx.beginPath();
             ctx.strokeStyle = fond;
-            ctx.lineWidth = 10; /*$('#selWidth').val();*/
+            ctx.lineWidth = $('#selWidth').val();;
             ctx.lineJoin = "round";
             ctx.moveTo(lastX, lastY);
             ctx.lineTo(x, y);
             ctx.closePath();
             ctx.stroke();
+
         }
         lastX = x;
         lastY = y;
@@ -109,7 +110,7 @@ $(document).ready(function() {
             // save image as png
             var link = document.createElement('a');
             link.download = "test.png";
-            link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");;
+            link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
             link.click();
         } else {
             alert("Please use Chrome");
@@ -120,7 +121,6 @@ $(document).ready(function() {
 
 
     $('#carre').click(function() {
-        var tool = this;
 
         $('#paint').mousedown(function(f) {
             originX = (f.pageX - this.offsetLeft);
@@ -129,7 +129,7 @@ $(document).ready(function() {
         });
 
         $('#paint').mousemove(function(f) {
-            if (mouseOn) {
+            if (mousePressed) {
                 endX = (f.pageX - this.offsetLeft) - originX;
                 endY = (f.pageY - this.offsetTop) - originY;
             }
@@ -146,13 +146,60 @@ $(document).ready(function() {
         });
 
         function draw() {
+            ctx.save();
             ctx.beginPath();
             ctx.strokeStyle = fond;
-            ctx.lineWidth = 10;
+            ctx.lineWidth = $('#selWidth').val();;
             ctx.lineJoin = "round";
             ctx.strokeRect(originX, originY, endX, endY, finalX, finalY);
             ctx.closePath();
             ctx.stroke();
+
+        };
+    });
+
+
+    $('#cercle').click(function() {
+
+
+
+        $('#paint').mousedown(function(g) {
+            originX = (g.pageX - this.offsetLeft);
+            originY = (g.pageY - this.offsetTop);
+            mousePressed = true;
+        });
+
+        $('#paint').mousemove(function(g) {
+            if (mousePressed) {
+                endX = (g.pageX - this.offsetLeft) - originX;
+                endY = (g.pageY - this.offsetTop) - originY;
+            }
+        });
+
+        $('#paint').mouseup(function(g) {
+            finalX = g.pageX - this.offsetLeft;
+            finalY = g.pageY - this.offsetTop;
+            circle();
+            mousePressed = false;
+        });
+        $('#paint').mouseleave(function(g) {
+            mousePressed = false;
+        });
+
+
+        function circle() {
+            ctx.save();
+            radius = Math.sqrt(Math.pow((originX - finalX), 2) + Math.pow((originY - finalY), 2));
+            ctx.beginPath();
+            ctx.strokeStyle = fond;
+            ctx.lineWidth = $('#selWidth').val();;
+            ctx.lineJoin = "round";
+            ctx.arc(originX, originY, radius, 0, (Math.PI * 2), true);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.save();
+
+            console.log(radius);
         };
     });
 });
